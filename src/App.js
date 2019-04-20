342294b8b40260dbdc44791145d507fb9c112c70
rmai99 <maidakrismarajagukguk@gmail.com>
2019-04-20 08:41:22 +0700

    tambah

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
       color : 'merah',
       desc : 'test',
       img : 'merah.jpg',
+      angka1 : '',
+      angka2 :'',
+      hasil :''
     };
   }
 
@@ -31,15 +34,16 @@ class App extends Component {
         <span className={"selected "+this.state.color}>{this.state.name}
         </span><br></br><br></br>
         <form onClick="">
-        <input type="number" id="number1" placeholder="Angka1"></input>
-        <select class="form-control" id="exampleFormControlSelect2">
+        <input type="number" ref="angka1" name="angka1" onChange={this.changeHandler.bind(this)} placeholder="Angka1"></input>+
+        {/*<select class="form-control" id="exampleFormControlSelect2">
           <option value="tambah">+</option>
           <option value="kali">*</option>
           <option value="kurang">-</option>
-          <option value="bagi">/</option>
-        </select>
-        <input type="number" id="number1" placeholder="Angka2"></input>
-        <button>Hitung</button>
+          <option value="bagi">/</option> <button>Hitung</button>
+      </select>*/}
+        <input type="number" ref="angka2" name="angka2" onChange={this.changeHandler1.bind(this)} placeholder="Angka2"></input>
+        <span>{this.state.hasil}</span>
+        
         </form>
       </div>)
     }else{
@@ -60,6 +64,26 @@ class App extends Component {
       <div className={"image "+ this.state.color}></div>*/}
       </div>)
     }
+  }      
+  changeHandler(event){
+    this.setState({
+      angka1:event.target.value
+    });
+    this.calculate();
+  }
+  changeHandler1(event){
+    this.setState({
+      angka2:event.target.value
+    });
+    this.calculate();
+  }
+  calculate(){
+    const a=parseInt(this.refs.angka1.value);
+    const b=parseInt(this.refs.angka2.value);
+    var hasil = a + b;
+    this.setState({
+      hasil
+    });
   }
   render(){
     return(
